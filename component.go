34b5660dbea6a2222b1d93fedc1e40498f132876
Roland Erk <roland.erk@conplement.de>
2025-01-28 10:33:12 +0100

    Add parent project uuid in component

--- component.go
+++ component.go
@@ -41,6 +41,7 @@ type Component struct {
 	DirectDependencies string              `json:"directDependencies,omitempty"`
 	Notes              string              `json:"notes,omitempty"`
 	ExternalReferences []ExternalReference `json:"externalReferences,omitempty"`
+	ProjectUUID        uuid.UUID           `json:"project.uuid,omitempty"`
 }
 
 type ExternalReference struct {
